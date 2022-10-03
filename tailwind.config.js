ef488cc946238f6d52b9354047baa861b08dd24d
kvarty <itlearning54@gmail.com>
2022-10-03 19:23:01 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "bg-gray":"#96A6C0",
+        "button-yellow": "#FFB373",
+        "text-gray": "#302F34",
+      },
+    },
+  },
+  plugins: [],
+}
